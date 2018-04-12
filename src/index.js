804adda867f0825e0300d2ae6c3ac7724a4fde29
Drew Parsons <drew@parsons.net>
2018-04-12 01:01:33 -0500

    random

--- src/index.js
+++ src/index.js
@@ -24,9 +24,9 @@ class HelloWorld extends React.Component {
 					Link
 				</NavItem>
 				<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-					<MenuItem eventKey={3.1}>Action</MenuItem>
+					<MenuItem eventKey={3.1}></MenuItem>
 					<MenuItem eventKey={3.2}>Another action</MenuItem>
-					<MenuItem eventKey={3.3}>Something else here</MenuItem>
+					<MenuItem eventKey={3.3}>Search </MenuItem>
 					<MenuItem divider />
 					<MenuItem eventKey={3.3}>Separated link</MenuItem>
 				</NavDropdown>
