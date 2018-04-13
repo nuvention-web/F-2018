4ce6e653e53bfc91d8eb3b28516e2d05947aae9c
Drew Parsons <drew@parsons.net>
2018-04-12 20:10:12 -0500

    Made shit look good

--- src/intro.js
+++ src/intro.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom'
 import './intro.css';
 import image from './static/intro.jpeg'
 
+var imageStyle = {
+  backgroundImage: `url(${image})`
+}
+
 class Head extends React.Component {
   render() {
     return (
-      <div className="img-container">
-        <img src={image}></img>
+      <div className="img-container" style={imageStyle}>
+        <div className="title-box">
+          <h1> tribe </h1>
+          <hr/>
+          <h5> A new career is within reach.</h5>
+          <h5> Let us Help you. </h5>
+        </div>
       </div>
     )
   }
