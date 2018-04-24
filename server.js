3466dba4b0e6e457781f466e92d1909836fdb7c3
Drew Parsons <andrewparsons2020@u.northwestern.edu>
2018-04-23 23:49:40 -0500

    asasdg

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
 });
 app.use(bodyParser.json());
 
-app.use('/', router);
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(cors());
 
 router.get('/', (req, res) => {
