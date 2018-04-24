fc06c611f9a0fdd2419f4d66a509ed9323cc0dd3
Bryan Li <bryanboyang@users.noreply.github.com>
2018-04-24 01:18:26 -0500

    Revert "Sprint 2"

--- src/App.js
+++ src/App.js
@@ -5,32 +5,17 @@ import TextField from 'material-ui/TextField';
 import axios from 'axios';
 
 
-
 class App extends Component {
     constructor(props) {
         super(props);
         this.key = 1;
         this.state = { text: '' };
         this.updateText = this.updateText.bind(this);
-        this.submitText = this.submitText.bind(this);
-        this.exampleApiCall = this.exampleApiCall.bind(this);
     }
 
 
     updateText = (e, value) => {
         this.setState({ text: [value] });
-    }
-
-
-    exampleApiCall = (e) => {
-        console.log("calling api")
-
-        axios.get('/api', {
-            text: 'api'
-        })
-        .then(function(res) {
-            console.log(res);
-        })
     }
 
     submitText = (e) => {
@@ -61,11 +46,6 @@ class App extends Component {
                         label="Submit" 
                         primary={true}
                         onClick={this.submitText} 
-                    />
-                    <FlatButton
-                        label="API Call"
-                        primary={true}
-                        onClick={this.exampleApiCall}
                     />
                 </div>
             </MuiThemeProvider>
