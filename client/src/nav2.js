7a6e9262f8d98364b9ed31bd0e0c0d2d700be852
Drew Parsons <andrewparsons2020@u.northwestern.edu>
2018-05-14 21:49:59 -0500

    fucked with mdb reat plugin for navbar but it's just not working

--- client/src/nav2.js
+++ client/src/nav2.js
@@ -28,45 +28,25 @@ export default class Nav2 extends React.Component {
 
     render() {
         return (
-            <Router>
-                <Navbar color="indigo" dark expand="md" scrolling>
+                <Navbar color="red" expand="lg">
                     <NavbarBrand href="/">
                         <strong>Navbar</strong>
                     </NavbarBrand>
                     { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
                     <Collapse isOpen = { this.state.collapse } navbar>
-                        <NavbarNav left>
-                          <NavItem active>
-                              <NavLink to="#">Home</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink to="#">Features</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink to="#">Pricing</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                              <DropdownToggle nav caret>Dropdown</DropdownToggle>
-                              <DropdownMenu>
-                                  <DropdownItem href="#">Action</DropdownItem>
-                                  <DropdownItem href="#">Another Action</DropdownItem>
-                                  <DropdownItem href="#">Something else here</DropdownItem>
-                                  <DropdownItem href="#">Something else here</DropdownItem>
-                              </DropdownMenu>
-                              </Dropdown>
-                          </NavItem>
-                        </NavbarNav>
-                        <NavbarNav right>
-                          <NavItem>
-                            <form className="form-inline md-form mt-0">
-                              <input className="form-control mr-sm-2 mb-0 text-white" type="text" placeholder="Search" aria-label="Search"/>
-                            </form>
-                          </NavItem>
+                        <NavbarNav>
+                        <NavItem active>
+                            <NavLink className="nav-link" to="#">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="#">Features</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="#">Pricing</NavLink>
+                        </NavItem>
                         </NavbarNav>
                     </Collapse>
                 </Navbar>
-            </Router>
         );
     }
 }
